ea278e906becea1d2183f62e2661243d08ff62d7
Christofer Bodin <christofer@christoferbodin.se>
2013-06-25 15:38:44 +0200

    Cleanup, theme support, pdo class, removed redbean, removed variables
    Cleanup of codebase, multiple theme support, pdo abstraction class with
    multi database support, removed RedBean from core, removed variables
    class so we're not depending on a specific db

--- index.php
+++ index.php
@@ -1,14 +1,15 @@
 <?php
+// start the session
 session_start();
+
+// send P3P Compact Policy (CP) headers to prevent IE from blocking cookies
 header('P3P: CP="CAO PSA OUR"');
 
-// define the root directory of the site (this files directory)
+// define the root directory of the site (this file's directory)
 define('SITE_ROOT', dirname(__FILE__));
 
-
 // include configuration
-require_once(SITE_ROOT . '/app/config.php');
-
+require_once(SITE_ROOT . '/app/config.inc');
 
 // Show all errors on development mode
 if (DEVELOPMENT) {
@@ -17,10 +18,6 @@ if (DEVELOPMENT) {
 else {
 	error_reporting(0);
 }
-
-
-// template directory
-define('TEMPLATE_DIR', SITE_ROOT . '/app/templates');
 
 // upload directory
 define('UPLOAD_DIR', SITE_ROOT . '/app/uploads');
@@ -34,38 +31,20 @@ define('PAGE_CALLBACK_AJAX', 1);
 // blank page view, only prints the content from the url callback
 define('PAGE_CALLBACK_BLANK', 2);
 
-
 // includes classes
 require_once(SITE_ROOT . '/core/inc/class.url.inc');
-require_once(SITE_ROOT . '/core/inc/class.variable.inc');
-require_once(SITE_ROOT . '/core/inc/class.core.inc');
+require_once(SITE_ROOT . '/core/inc/class.session.inc');
+require_once(SITE_ROOT . '/core/inc/class.database.inc');
 require_once(SITE_ROOT . '/core/inc/class.template.inc');
-
-// include libraries
-require_once(SITE_ROOT . '/core/libs/redbeanphp/rb.php');
+require_once(SITE_ROOT . '/core/inc/class.core.inc');
 
 // include modules
 if (FACEBOOK) {
 	require_once(SITE_ROOT . '/core/modules/facebook/facebook.inc');
 }
 
-// include the app class
+// include the app file
 require_once(SITE_ROOT . '/app/app.inc');
 
-// setup database connection
-R::setup('mysql:host='. DB_HOST .';dbname='. DB_NAME, DB_USER, DB_PASS);
-
-if (DEVELOPMENT) {
-	$queryLogger = RedBean_Plugin_QueryLogger::getInstanceAndAttach(R::getDatabaseAdapter());
-}
-else {
-	R::freeze(true);
-}
-
 Core::init();
-
-if (DEVELOPMENT) {
-	Core::add_html_variables(array('query_logs' => $queryLogger->getLogs()));
-}
-
 Core::view();
