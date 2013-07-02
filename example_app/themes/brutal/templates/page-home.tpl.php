58ab60b5e0e9072ec8205f0c236656bba624cd98
Christofer Bodin <christofer@christoferbodin.se>
2013-07-02 23:13:40 +0200

    Updated example app with logo and redbeanphp module

--- example_app/themes/brutal/templates/page-home.tpl.php
+++ example_app/themes/brutal/templates/page-home.tpl.php
@@ -1,1 +1,1 @@
-Homepage
+<a href="https://github.com/cbodin/brutal-php-framework">https://github.com/cbodin/brutal-php-framework</a>
--- example_app/themes/brutal/templates/page.tpl.php
+++ example_app/themes/brutal/templates/page.tpl.php
@@ -1,4 +1,7 @@
 <div id="wrapper">
+	<div id="header">
+		<img src="<?php print Url::generate_theme('images/logo.svg'); ?>">
+	</div>
 	<div id="content">
 		<?php print $content; ?>
 	</div>
